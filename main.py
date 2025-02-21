180f258739bf3034edb38eb21a980718afa0fe5c
Nero15739 <andrewpajva@gmail.com>
2025-02-21 12:50:52 +1100

    Word Counting function

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def main ():
     path_to_file = "Books/frankenstein.txt"
     with open(path_to_file) as f:
         file_contents = f.read()
-        print(file_contents)
+        num = countWords(file_contents)
+        print(f"Word Count : {num}")
 
+
+
+def countWords(words):
+    arr = words.split(' ')
+    # for str in arr:
+    #     print(str)
+    return len(arr)
+    
 main()

e751f4f24af531942152ae918155229b8a9eb4a3
Nero15739 <andrewpajva@gmail.com>
2025-02-21 22:32:35 +1100

    Char Count dictionary

--- main.py
+++ main.py
@@ -4,11 +4,32 @@ def main ():
         file_contents = f.read()
         num = countWords(file_contents)
         print(f"Word Count : {num}")
+        charCounts = countChars(file_contents)
+        #Print char counts iterating through the dictionary
+        print(charCounts)
 
 
-
+# Counts the individual words that are separated by any white space or new line, tab etc..
 def countWords(words):
     arr = words.split()
     return len(arr)
+
+#Counts the chars in the alphabet after being lowered. then returns the dictionary
+#ARGS: string file_contents
+#RETURNS: Dictionary char counts
+def countChars(file_contents) :
+    charCounts = {}
+    for char in file_contents:
+        lowerChar = char.lower()
+        if lowerChar in charCounts.keys():
+            charCounts[lowerChar] = charCounts[lowerChar] + 1
+        else:
+            charCounts[lowerChar] = 1
+            # print(lowerChar)
+
+    return charCounts
+    
+
+    
     
 main()
